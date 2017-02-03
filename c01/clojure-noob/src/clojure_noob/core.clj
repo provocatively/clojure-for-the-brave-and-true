07bb331dc4d1e1ddb5cfd040a69dbbf5f1db0dcb
邓 <1351650853@qq.com>
2017-02-03 16:07:49 +0800

    the first project

---
+++ c01/clojure-noob/src/clojure_noob/core.clj
@@ -0,0 +1,10 @@
+; run this program in terminal with "lein run"
+
+(ns clojure-noob.core                                       ; declare a namespace
+  (:gen-class))
+
+
+(defn -main                                                 ; entry point to program
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "I'm a little teapot!"))
